3698d32b63dd24a0763b694158f905ad216c4e42
Anastasiia Tyustina <103989119+Tyustina@users.noreply.github.com>
2024-06-27 19:24:52 +0500

    Добавляет закрытие информационных сообщений по ТЗ

--- js/message-response.js
+++ js/message-response.js
@@ -3,45 +3,31 @@ import { resetEditingForm } from './form-upload.js';
 
 const successMessageTemplate = document.querySelector('#success').content.querySelector('.success');
 const errorMessageTemplate = document.querySelector('#error').content.querySelector('.error');
-const modalElementBackground = document.querySelector('.overlay');
-const successMessageBackground = document.querySelector('.success');
-const errorMessageBackground = document.querySelector('.error');
 let currentMessage;
 
-const getUploadMessage = (template, button) => { //получение шаблона всплывающего сообщения и закрытие его по ТЗ
+const getUploadMessage = (template) => { //получение шаблона всплывающего сообщения и закрытие его по ТЗ
   currentMessage = template.cloneNode(true);
-  const messageButtonClose = currentMessage.querySelector(button);
   bodyPage.append(currentMessage);
-  messageButtonClose.addEventListener('click', closeUploadMessage);//закрытие по крестику
-  bodyPage.addEventListener('keydown', onDocumentKeydown);//закрытие по Esc
+  // bodyPage.addEventListener('keydown', onDocumentKeydown);//закрытие по Esc
+  bodyPage.addEventListener('click', closeUploadMessage);
 };
 
 export const showUploadSuccessMessage = () => {
-  getUploadMessage(successMessageTemplate, '.success__button');
+  getUploadMessage(successMessageTemplate);
   resetEditingForm();
 };
 
 export const showUploadErrorMessage = () => {
-  getUploadMessage(errorMessageTemplate, '.error__button');
+  getUploadMessage(errorMessageTemplate);
 };
 
-function closeUploadMessage() {
-  currentMessage.remove();
-  bodyPage.removeEventListener('keydown', onDocumentKeydown);
-}
-
-function onDocumentKeydown(evt) {
+const closeUploadMessage = (evt) => {
   evt.stopPropagation();
-  if (evt.key === 'Escape') {
-    closeUploadMessage();
+  const existElement = document.querySelector('.success')|| document.querySelector('.error');
+  const closeButton = existElement.querySelector('button');
+  if(evt.target === existElement || evt.target === closeButton || evt.key === 'Escape'){
+    existElement.remove();
+    bodyPage.removeEventListener('keydown', closeUploadMessage);
+    bodyPage.removeEventListener('click', closeUploadMessage);
   }
 }
-
-
-/* При выпадении сообщения о ошибке загрузки
-нажатием на кнопку пропадает сообщение, но само окно остается и можно отправить повторно
-если же нажать Esc то закрывается все насовсем
-по ТЗ у пользователя должна быть возможность сохранить введеные данные, то есть при нажатии на Esc (при сообщении об ошибке) должно закрываться не все окно загрузки - а лишь сообщение об ошибке
-При сообщении о успешной загрузке при любом способе закрытия должно закрываться все
-как это реализовать??
-*/
